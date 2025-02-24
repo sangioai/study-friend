26745a4c54e0183f39493a877d716ce769b9af64
SangioAI <marcosangio03@gmail.com>
2025-02-24 23:31:44 +0100

    fix: bitsandbytes dependency

--- setup.py
+++ setup.py
@@ -14,8 +14,10 @@ sys.path.append(str(package_dir))
 # Read the requirements from the requirements.txt file
 requirements_path = root_dir / "requirements.txt"
 with open(requirements_path) as fid:
-    # exclude mlx-vlm only if non-MLX device
-    requirements = [l.strip() for l in fid.readlines()  if not l.startswith("mlx-vlm") or torch.mps.is_available()]
+    # exclude mlx-vlm only on non-MLX device
+    requirements = [l.strip() for l in fid.readlines()  if not l.startswith("mlx-vlm")      or torch.mps.is_available()]
+    # exclude bitsandbytes on non-CUDA device
+    requirements = [l.strip() for l in requirements     if not l.startswith("bitsandbytes") or torch.cuda.is_available()]
 
 # Setup configuration
 setup(
